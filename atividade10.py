e1b95fb9326c01d3b3a1af5736e46cf27ff5dcf9
Aderlangio123 <faderlangio123@gmail.com>
2024-09-11 14:28:55 -0300

    atividade 10 - verificar se numero e impar ou par

---
+++ atividade10.py
@@ -0,0 +1,11 @@
+# Crie um programa que receba um número inteiro e informe se ele é par ou ímpar.
+
+numero = int(input("digite seu numero! :  "))
+
+if (numero % 2) == 0:
+    print(f"o numero  {numero} é par")
+else:
+    print(f"o numero {numero} é impar")
+
+    
+    
